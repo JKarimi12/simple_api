49e460ad4bf330b9678e0a9803c5d87f593e3c07
javad.karimi <karimijavad544@gmail.com>
2023-07-30 00:51:02 +0330

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/test/', methods=['POST'])
+def mirror_body():
+    return json.loads(request.data)
